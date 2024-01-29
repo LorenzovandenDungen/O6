45074dd7194b800d0bf42b391293f7b23af5ea5e
LorenzovandenDungen <2130766@talnet.nl>
2024-01-29 13:40:27 +0100

    save

--- telaat/config/web.php
+++ telaat/config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Swoucu6YRvV8sYNg_CLqmrCAja4IltUB',
+            'cookieValidationKey' => 'LD-1q96YeWZN9yuKQcuXh4z1k61ZCFGv',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
