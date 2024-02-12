9039391067934948cadd5f964a170ad248246398
LorenzovandenDungen <2130766@talnet.nl>
2024-02-12 13:43:55 +0100

    save

--- O6 - Oefenen met Events/lorenzo.js
+++ O6 - Oefenen met Events/lorenzo.js
@@ -1,16 +1,23 @@
+//klik op de tab HTML om de HTML code te zien en eventueel aan te passen!
+
 function myFunction(){
-    let today = new Date();
-    alert(today.toLocaleDateString());
+  //laat de datum van vandaag op het scherm zien met een alert box 
+  var today = new Date();
+  alert("Vandaag is het: " + today.toDateString());
 }
 
-function colorIt(el, color){
-    el.style.color = color;
+function colorIt(el){
+  //verander de tekstkleur van el
+  el.style.color = "blue";
 }
 
 function changeText(id, text){
-    document.getElementById(id).innerHTML = text;
+ //verander de tekst met de id: info in jouw naam, gebruik voor de id en je naam de gegevens die je aan de functie hebt meegegeven, check ik de tab HTML of alles klopt...
+  document.getElementById(id).innerHTML = text;
 }
 
-function hoverIt(id, color){
-    document.getElementById(id).style.backgroundColor = color;
+function hoverIt(){
+  //verander de achtergrond kleur van het blokje met de id: blokje, naar een willekeurige kleur
+  var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  document.getElementById("blokje").style.backgroundColor = randomColor;
 }
